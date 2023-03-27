84ce613f366c299069fd2db2a472be1672b7f1f6
Alexander Holm <alho0014@student.hv.se>
2023-03-27 11:54:26 +0200

    Added main menu icons

---
+++ build/mod.cpp
@@ -0,0 +1,8 @@
+name = "Loot To Vehicle";
+author = "CseRoQ";
+
+logo		 = "\LootToVehicle\images\icon.paa"; 	        // Logo displayed in the main menu
+logoOver	 = "\LootToVehicle\images\icon_hover.paa";      // When the mouse is over, in the main menu
+
+//picture 	 = "icons\icon_128.paa"; 	// Picture displayed from the expansions menu. Optimal size is 2048x1024
+//overview = "mod.cpp Overview text";     // Expansions menu text
